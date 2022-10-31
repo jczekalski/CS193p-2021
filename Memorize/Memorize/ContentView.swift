a29e4774ade16dad3e344c64f4b4243559aa7bf1
jczekalski <john.czekalski@gmail.com>
2022-10-31 18:20:02 +0100

    init

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by JC on 31/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, CS193p!")
+            .padding()
+            
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
