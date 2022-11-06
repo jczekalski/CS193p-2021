59dfe6cf3a932ae2b5ec5287813af96a5675425b
jczekalski <john.czekalski@gmail.com>
2022-11-06 01:08:33 +0100

    lecture 6: protocols, shapes

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -13,7 +13,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["💻", "🐢", "✈️", "🦀", "🦄", "🦇", "🐋", "⛄️", "🪐", "☂️", "🍏", "🍄"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 10) { pairIndex in
             emojis[pairIndex]
         }
     }
