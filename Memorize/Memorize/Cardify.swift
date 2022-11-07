731432f5da36da038dac85abcd2b1547510d5163
jczekalski <john.czekalski@gmail.com>
2022-11-07 21:58:18 +0100

    lecture 7: ViewModifier, animations part 1: implicit animations (deprecated in iOS 15)

---
+++ Memorize/Memorize/Cardify.swift
@@ -0,0 +1,37 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by JC on 07/11/2022.
+//
+
+import SwiftUI
+
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+            } else {
+                shape.fill(.red)
+            }
+            content.opacity(isFaceUp ? 1 : 0)
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        return self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
